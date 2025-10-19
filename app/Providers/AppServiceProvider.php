9de1646d3a2dd4331bb415b767047bfd00652bbc
mfadev22 <mfadev223@gmail.com>
2025-10-19 19:37:18 +0700

    appService Update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Carbon;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Paksa timezone Laravel dan PHP sesuai config/app.php
+        date_default_timezone_set(Config::get('app.timezone'));
+
+        // Pastikan Carbon juga sinkron dengan timezone dan locale
+        Carbon::setLocale('id');
+        Carbon::setDefaultTimezone(Config::get('app.timezone'));
     }
 }
